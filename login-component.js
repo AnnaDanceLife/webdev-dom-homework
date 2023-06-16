0cbdb4fd77c2cd3b0db74ef8edf496d7bdff3a71
Anna Shatilova <anxet@mail.ru>
2023-06-16 18:56:52 +0300

    Добавляет форму регистрации

--- login-component.js
+++ login-component.js
@@ -1,25 +1,38 @@
 import { loginAuth } from "./api.js";
 
 export function renderLoginComponent(appEl, commentsHtml) {
-    const appHtml = `
-<div class="container">
-<ul id="comments" class="comments">
-${commentsHtml}
-</ul>
-  <div class="add-form">
-      <h3 class="title">Форма входа</h3>
-          <input type="text" class="add-form-name add-form-login" placeholder="Введите логин" id="login"/> <br>
-          <input type="password" class="add-form-name" placeholder="Введите пароль" id="password"/>
-      <button id="auth-button" class="auth-button add-form-button">Войти</button>
-      <button id="auth-toggle-button" class="add-form-button auth-button auth-toggle">Зарегистрироваться</button>
-  </div>
-  </div>`
+    let isLoginMode = true;
+    const renderForm = () => {
+        const appHtml = `
+        <div class="container">
+        <ul id="comments" class="comments">
+        ${commentsHtml}
+        </ul>
+          <div class="add-form">
+              <h3 class="title">Форма ${isLoginMode ? "входа" : "регистрации"}</h3>
+              ${isLoginMode
+                ? ""
+                : `
+                <input type="text" class="add-form-name add-form-login" placeholder="Введите имя" id="name"/> <br>`
+            }
+                <input type="text" class="add-form-name add-form-login" placeholder="Введите логин" id="login"/> <br>
+                <input type="password" class="add-form-name" placeholder="Введите пароль" id="password"/>
+              <button id="auth-button" class="auth-button add-form-button">${isLoginMode ? "Войти" : "Зарегистрироваться"}</button>
+              <button id="toggle-button" class="add-form-button auth-button auth-toggle">Перейти ${isLoginMode ? "к регистрации" : "ко входу"}</button>
+          </div>
+          </div>`
 
-    appEl.innerHTML = appHtml;
+        appEl.innerHTML = appHtml;
 
-    const authButton = document.getElementById('auth-button');
-    authButton.addEventListener('click', () => {
+        const authButton = document.getElementById('auth-button');
+        authButton.addEventListener('click', () => {
+            loginAuth();
+        });
 
-        loginAuth();
-    })
+        document.getElementById('toggle-button').addEventListener('click', () => {
+            isLoginMode = !isLoginMode;
+            renderForm();
+        })
+    }
+ renderForm();
 }
